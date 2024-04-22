b5df0b912838f9768061471e1022df83732e7fbb
swagd3v <maxey127@hotmail.com>
2024-04-22 18:20:12 -0500

    Added ui

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 const choices = ["rock", "paper", "scissors"];
-let result;
-let = playerSelection = prompt("rock, papper, or scissors?").toLowerCase();
+let playerScore = 0;
+let computerScore = 0;
 let computerSelection = getComputerChoice();
+let rock = document.getElementById("rock");
+let paper = document.getElementById("paper");
+let scissors = document.getElementById("scissors");
+rock.addEventListener("click", () => {
+  playRound("rock", getComputerChoice());
+});
+paper.addEventListener("click", () => {
+  playRound("paper", getComputerChoice());
+});
+scissors.addEventListener("click", () => {
+  playRound("scissors", getComputerChoice());
+});
 
 function getComputerChoice() {
   let random = Math.trunc(Math.random() * 3);
@@ -9,6 +21,7 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
+  let result = "";
   if (playerSelection === computerSelection) {
     result = "ITS A TIE";
   } else {
@@ -25,15 +38,36 @@ function playRound(playerSelection, computerSelection) {
     }
   }
 
-  return result;
-}
+  const playerChoice = document.getElementById("player-choice");
+  const computerChoice = document.getElementById("computer-choice");
+  playerChoice.innerText = `PLAYER: ${playerSelection}`;
+  computerChoice.innerText = `COMPUTER: ${computerSelection}`;
 
-function playGame() {
-  playRound(playerSelection, computerSelection);
-  playRound(playerSelection, computerSelection);
-  playRound(playerSelection, computerSelection);
-  playRound(playerSelection, computerSelection);
-  playRound(playerSelection, computerSelection);
-}
+  const playerDisplay = document.getElementById("player-display");
+  const computerDisplay = document.getElementById("computer-display");
 
-playGame();
+  function reset() {
+    playerScore = 0;
+    computerScore = 0;
+    playerDisplay.innerText = "Player:";
+    computerDisplay.innerText = "Computer:";
+  }
+
+  if (result === "You WIN") {
+    playerScore++;
+    playerDisplay.innerText = `Player: ${playerScore}`;
+    if (playerScore >= 5) {
+      alert("YOU WIN");
+      reset();
+    }
+  } else if (result === "You LOSE") {
+    computerScore++;
+    computerDisplay.innerText = `Computer: ${computerScore}`;
+    if (computerScore >= 5) {
+      alert("YOU LOSE");
+      reset();
+    }
+  } else {
+    return;
+  }
+}
